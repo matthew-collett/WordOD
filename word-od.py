d1315c296651b2384388d208b8871664d4fb9ac2
matthew-collett <97645707+matthew-collett@users.noreply.github.com>
2022-12-30 12:34:12 -0400

    Fix update response bug

--- word-od.py
+++ word-od.py
@@ -6,21 +6,22 @@ import discord
 from bs4 import BeautifulSoup
 from datetime import date
 
-URL = "https://www.dictionary.com/e/word-of-the-day/"
-RESP = requests.get(URL)
-SOUP = BeautifulSoup(RESP.content, "html.parser")
-
 #load config
 with open("config.json", "r") as file:
     config = json.load(file)
 
 WEBHOOK = config["webhook_url"]
 
+URL = "https://www.dictionary.com/e/word-of-the-day/"
+
 def get_word_od_msg():
-    word = SOUP.find('h1', class_='js-fit-text').text.strip()
-    pronun = SOUP.find('span', class_='otd-item-headword__pronunciation__text').text.strip().replace(" ", "")
-    qualities = SOUP.find('div', class_='otd-item-headword__pos').text.strip().split("\n\n")
-    sentence = SOUP.find('div', class_='wotd-item-origin').findAll('p')[2].text.strip()
+    resp = requests.get(URL)
+    soup = BeautifulSoup(resp.content, "html.parser")
+
+    word = soup.find('h1', class_='js-fit-text').text.strip()
+    pronun = soup.find('span', class_='otd-item-headword__pronunciation__text').text.strip().replace(" ", "")
+    qualities = soup.find('div', class_='otd-item-headword__pos').text.strip().split("\n\n")
+    sentence = soup.find('div', class_='wotd-item-origin').findAll('p')[2].text.strip()
     
     msg = "@everyone\nWord of the day for {}\n\n**{}**\n*{}*\n*{}*\n\n**DEFINITION**\n*{}*\n\n*{}*".format(date.today().strftime("%B %d, %Y"), word.upper(), qualities[0].rstrip(), pronun, qualities[1], sentence)
     return msg
@@ -29,7 +30,7 @@ def send_word_od_msg():
     msg = get_word_od_msg()
     discord.SyncWebhook.from_url(WEBHOOK).send(msg)
 
-schedule.every().day.at("9:00").do(send_word_od_msg)
+schedule.every().day.at("8:00").do(send_word_od_msg)
 
 while True:
     schedule.run_pending()
