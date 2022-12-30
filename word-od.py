e7f76021f931901eb1142f8432517e58b1b9c1a1
matthew-collett <97645707+matthew-collett@users.noreply.github.com>
2022-12-29 21:28:26 -0400

    Initial commit

---
+++ word-od.py
@@ -0,0 +1,36 @@
+import requests
+import schedule
+import time
+import json
+import discord
+from bs4 import BeautifulSoup
+from datetime import date
+
+URL = "https://www.dictionary.com/e/word-of-the-day/"
+RESP = requests.get(URL)
+SOUP = BeautifulSoup(RESP.content, "html.parser")
+
+#load config
+with open("config.json", "r") as file:
+    config = json.load(file)
+
+WEBHOOK = config["webhook_url"]
+
+def get_word_od_msg():
+    word = SOUP.find('h1', class_='js-fit-text').text.strip()
+    pronun = SOUP.find('span', class_='otd-item-headword__pronunciation__text').text.strip().replace(" ", "")
+    qualities = SOUP.find('div', class_='otd-item-headword__pos').text.strip().split("\n\n")
+    sentence = SOUP.find('div', class_='wotd-item-origin__content wotd-item-origin__content-full').findAll('p')[2].text.strip()
+
+    msg = "@everyone\n__Word of the day for {}__\n\n**{}**\n*{}*\n*{}*\n\n**DEFINITION**\n*{}*\n\n*{}*".format(date.today().strftime("%B %d, %Y"), word.upper(), qualities[0], pronun, qualities[1], sentence)
+    return msg
+
+def send_word_od_msg():
+    msg = get_word_od_msg()
+    discord.SyncWebhook.from_url(WEBHOOK).send(msg)
+
+schedule.every().day.at("08:00").do(send_word_od_msg)
+
+while True:
+    schedule.run_pending()
+    time.sleep(60) # wait one minute
